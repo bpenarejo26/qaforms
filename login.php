680dc2b25eb728f431b38468cc248bbd6090583c
bpenarejo26 <bpenarejo.github@belusallc.com>
2016-11-03 09:43:52 -0400

    hash and salt
    update hash and salt log-in for upgrade high level security

--- login.php
+++ login.php
@@ -53,6 +53,7 @@ table {
 
 <?php 
 include("connection.php");
+include ("function.php");
 ob_start();
 session_start();
 if($_SERVER["REQUEST_METHOD"] == "POST")	
@@ -67,28 +68,34 @@ if(isset($_POST['search'])) {
 $user  = $_POST['user'];
 $pass = $_POST['pass'];
 
-$sql = "select EmpID, Fullname, Disable, Manager, Level from employee where Username = '$user' and Password = '$pass'";
+$sql = "select EmpID, Fullname, Disable, Manager, Password, Level from employee where Username = '$user'";
 $result = mysqli_query($connect, $sql);
 $row = mysqli_fetch_array($result);
 $countrow = mysqli_num_rows($result);
+if($row['Disable'] == "0") {echo "Your account is not active. Please contact your IT administrator";}
+if (password_verify($pass, $row['Password']) == TRUE) {
 //echo $countrow;
-if($countrow <= 0) {
-echo "Please check username and password!";
-}
-else {
-	if(($row['Level'] == 1) && ($row['Disable'] == TRUE)){
-	echo $row['Disable']; 
+if(($row['Level'] == 1) && ($row['Disable'] == TRUE)){
+	//echo $row['Disable']; 
 	$id = $row['EmpID']; $y = $row['Manager'];
-	header("Location:http://10.16.1.102:8085/qaforms/admin.php?id=".$id);						
+	header("Location:http://10.16.1.102:8082/branders/admin.php?id=".$id);						
 							}
 							else
 
 							{
+								$auto = generate();
 								if($row['Disable'] == TRUE) {
-								$id = $row['EmpID'];  $id2 = $row['Manager']; header("Location:http://10.16.1.102:8085/qaforms/qa.php?id=".$id);
+								$id = $row['EmpID'];  $id2 = $row['Manager']; header("Location:http://10.16.1.102:8082/branders/QA.php?id=".$id."&x=".$auto);
+								//echo generate();
+
+$insert = "INSERT INTO Logbook (`TransNo`, `ID`, `Time-in`, `Ref`, `Time-out`) VALUES ('', '$id', CURRENT_TIMESTAMP, '$auto', '0000-00-00 00:00:00.000000')";
+$result = mysqli_query($connect,$insert);
 								}
 							}
+
+} else {
+echo "Please check username and password!";	
 }
-if($row['Disable'] == "0") {echo "Your account is not active. Please contact your IT administrator";}
 }
+
 ?>
