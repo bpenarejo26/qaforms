b1fa282b6bb9f1c1f7e801e8317c680f12f9af5d
bpenarejo26 <bpenarejo.github@belusallc.com>
2016-12-01 13:46:20 -0500

    with miami
    update

--- newpw.php
+++ newpw.php
@@ -17,12 +17,54 @@ table {
 	margin: 0.01em auto;
 	text-align: left;
 }
+ul{
+        padding: 0;
+        list-style: none;
+        background: #1375bc;
+    }
+    ul li{
+        display: inline-block;
+        position: relative;
+        line-height: 21px;
+        text-align: left;
+    }
+    ul li a{
+        display: block;
+        padding: 8px 25px;
+        color: #FFFFFF;
+        text-decoration: none;
+    }
+    ul li a:hover{
+        color: #fff;
+        background: #939393;
+    }
+    ul li ul.dropdown{
+        min-width: 100%; /* Set width of the dropdown */
+        background: #1375bc;
+        display: none;
+        position: absolute;
+        z-index: 999;
+        left: 0;
+    }
+    ul li:hover ul.dropdown{
+        display: block;	/* Display the dropdown */
+    }
+    ul li ul.dropdown li{
+        display: block;
+    }
 
 </style>
 <form name ="admin" method="POST"/>
 <meta charset="utf-8">
 <title>BEL USA - Manila QA Form</title>
 </head>
+<ul>
+        <li><a href="qa.php?id=<?php echo $id;?>&x=<?php echo $auto;?>"tabindex=1>Home</a></li>
+       
+        <li> <a href="dater_all.php?id=<?php echo $id;?>&x=<?php echo $auto;?>"tabindex=-1>Report </a></li>
+			 <li><a href="newpw.php?id=<?php echo $id;?>&x=<?php echo $auto;?>"tabindex=-1>Change password</a></li>
+			 <li><p align="right"><input type = "submit" name = "out" value = "Log-out" tabindex=-1/> </p></li>
+			</ul>
 <?php $id = $_GET['id']; //echo $id;?>
 <body>
 <table width="30%" border="0">
@@ -50,7 +92,7 @@ table {
       <th height="100" colspan="2" scope="col"><img  class = "center fit" src="1-02.jpg" > </th>
     </tr>
   </tbody>
-  <a align= "center" href="login.php">Back to Log-in</a>
+
 </table>
 </form>
 </body>
@@ -81,6 +123,7 @@ if (((password_verify($old, $row['password']) == TRUE) && mysqli_num_rows($resul
 $pass = password_hash($_POST['pass1'], PASSWORD_BCRYPT, array("cost" => 10));
 $update = "UPDATE employee SET Password='$pass' WHERE EmpID = '$id'";
 $result = mysqli_query($connect, $update);
+//echo $pass;
 echo "You account has been updated with new password! Please re log-in your account";
 }
 else
@@ -88,5 +131,13 @@ else
 }
 
 }
-
+if(isset($_POST['out'])){
+	$EasternTimeStamp =mktime(date('H')-6,date('i'),date('s'),date("m"),date("d"),date("Y"));
+	$udate = date('Y-m-d H:i:s',$EasternTimeStamp );
+	$update = "update logbook set `Time-out` = '$udate' where Ref like '%$auto%' ";
+	$result = mysqli_query($connect, $update);
+	session_destroy();	
+	header("Location:login.php");
+	}
+	
 ?>
