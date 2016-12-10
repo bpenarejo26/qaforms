babb7ae00f81f2b33cdb36d055b2283645f539e3
bpenarejo26 <bpenarejo.github@belusallc.com>
2016-12-10 00:41:40 -0500

    removed time
    removed time

--- sample.php
+++ sample.php
@@ -1,12 +1,12 @@
 <?php
-$conn = mysql_connect("localhost","root","Fic5#w0F");
+$conn = mysql_connect("localhost","itsupport","Fic5#w0F");
 mysql_select_db("branders",$conn);
 $_SERVER["REQUEST_METHOD"] == "POST";
 $d1 = $_GET['d1'];
 $d2 = $_GET['d2'];
-//echo $d1;
-//echo $d2;
-$filename = "BEL_USA_QA_Form.csv";
+$manager = $_GET['manager'];
+
+$filename = "BEL_USA_QA_Formv2.csv";
 $fp = fopen('php://output', 'w');
 
 $header = array("Id", "ORNo", "Assoc", "Manager", "Checker", "EvalDate", "Accuracy", "CustIns", "OveRep", "TexSiz", "PosNeg", "Size", "Loca", "Colo", "ArtOrd", "AdjArt", "MocUp", "ArtSiz", "TemLay", "OrdDet", "ImpSiz", "ArtBox", "MocBox", "UplFil", "BacSli", "Und", "SVG", "UplPro", "AddNote", "EID", "EDate");
@@ -23,9 +23,9 @@ fputcsv($fp, $header);
 
 $num_column = count($header);		
 //$query = "SELECT `TransNo`, `ORNo`, `Assoc`, `Manager`, `Checker`, `EvalDate`, `Accuracy`, `CustIns`, `OveRep`, `TexSiz`, `PosNeg`, `Size`, `Loca`, `Colo`, `ArtOrd`, `AdjArt`, `MocUp`, `ArtSiz`, `TemLay`, `OrdDet`, `ImpSiz`, `ArtBox`, `MocBox`, `UplFil`, `BacSli`, `Und`, `SVG`, `UplPro`, `AddNote`,  `TransID`, `EDate`FROM `qualitycheck` WHERE EvalDate BETWEEN '$d1' and '$d2'";
-$sql = "SELECT `TransNo`, `ORNo`, `Source`, `Brand`, `Assoc`, `Manager`, `Checker`, `EvalDate`, `Accuracy`, `CustIns`, `OveRep`, `TexSiz`, `PosNeg`, `Size`, `Loca`, `Colo`, `ArtOrd`, `AdjArt`, `MocUp`, `ArtSiz`, `TemLay`, `OrdDet`, `ImpSiz`, `ArtBox`, `MocBox`, `UplFil`, `BacSli`, `Und`, `UplPro`, `AddNote`, `SVG`, `EDate`, `TransID`  FROM `qualitycheck`WHERE EvalDate BETWEEN '$d1' and '$d2' and Manager = '$manager'";
+$sql = "SELECT `TransNo`, `ORNo`, `Assoc`, `Manager`, `Checker`, `EvalDate`, `Accuracy`, `CustIns`, `OveRep`, `TexSiz`, `PosNeg`, `Size`, `Loca`, `Colo`, `ArtOrd`, `AdjArt`, `MocUp`, `ArtSiz`, `TemLay`, `OrdDet`, `ImpSiz`, `ArtBox`, `MocBox`, `UplFil`, `BacSli`, `Und`, `UplPro`, `AddNote`, `SVG`, `EDate`, `TransID`  FROM `qualitycheck`WHERE EvalDate BETWEEN '$d1' and '$d2' and Team = '$manager'";
 
-$result = mysql_query($query);
+$result = mysql_query($sql);
 while($row = mysql_fetch_row($result)) {
 	fputcsv($fp, $row);
 }
