da782793e6db6ed27f0a9cae2edb73e03c2ab272
bpenarejo26 <bpenarejo.github@belusallc.com>
2016-12-10 01:35:55 -0500

    fixed login time
    up

--- login.php
+++ login.php
@@ -72,13 +72,15 @@ $sql = "select EmpID, Fullname, Disable, Manager, Password, Level from employee 
 $result = mysqli_query($connect, $sql);
 $row = mysqli_fetch_array($result);
 $countrow = mysqli_num_rows($result);
+$EasternTimeStamp =mktime(date('H')-5,date('i'),date('s'),date("m"),date("d"),date("Y"));
+$date = date('Y-m-d H:i:s',$EasternTimeStamp );
 if($row['Disable'] == "0") {echo "Your account is not active. Please contact your IT administrator";}
 if (password_verify($pass, $row['Password']) == TRUE) {
 //echo $countrow;
 if(($row['Level'] == 1) && ($row['Disable'] == TRUE)){
 	//echo $row['Disable']; 
 	$id = $row['EmpID']; $y = $row['Manager'];
-	header("Location:http://localhost/branders/admin.php?id=".$id);						
+	header("Location:http:admin.php?id=".$id);						
 							}
 							else
 
@@ -88,7 +90,7 @@ if(($row['Level'] == 1) && ($row['Disable'] == TRUE)){
 								$id = $row['EmpID'];  $id2 = $row['Manager']; header("Location:http:qa.php?id=".$id."&x=".$auto);
 								//echo generate();
 
-$insert = "INSERT INTO Logbook (`TransNo`, `ID`, `Time-in`, `Ref`, `Time-out`) VALUES ('', '$id', CURRENT_TIMESTAMP, '$auto', '0000-00-00 00:00:00.000000')";
+$insert = "INSERT INTO logbook (`TransNo`, `ID`, `Time-in`, `Ref`, `Time-out`) VALUES ('', '$id', '$date', '$auto', '0000-00-00 00:00:00.000000')";
 $result = mysqli_query($connect,$insert);
 								}
 							}
